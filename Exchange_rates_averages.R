35d10ef0845a20487256106b31c62a6d1f0dff39
Casputin <39671075+Casputin@users.noreply.github.com>
2018-06-11 11:22:13 +0200

    Added custom mean function (because nested)

--- Exchange_rates_averages.R
+++ Exchange_rates_averages.R
@@ -1,6 +1,12 @@
 library(dplyr)
 library(lubridate)
+library(zoo)
+
 
 exchange_rates <- readRDS("C:/Users/cenge/Desktop/Money Credit Finance/Paper/bc_commodities/data/exchange_rates.rds")
-quarter(exchange_rates$Date)
-year(exchange_rates$Date)
+
+custommean<- function(x){mean(x, na.rm=T)}
+
+means<- aggregate(exchange_rates[,2:7], list(exchange_rates$Date2),custommean)
+
+
