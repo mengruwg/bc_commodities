b1d29c139be2d0b3d7af4f77b05ffeb211ee683e
Casputïn <cengelen0@gmail.com>
2018-06-16 16:27:27 +0200

    PCA done

--- PCA_process.R
+++ PCA_process.R
@@ -3,10 +3,12 @@ library(zoo)
 library(dplyr)
 library(REdaS)
 library(psych)
-
-datCOM <- readRDS("C:/Users/cenge/Desktop/Money Credit Finance/Paper/bc_commodities/data/commodities/datCOM.RDS")
+library(readr)
+getwd()
+datCOM <- readRDS("data/commodities/datCOM.RDS")
 #ohne futures
 datCOM2<-subset(datCOM, select= -c(Brent1yr, Brent2yr, Brent3yr, Brent6m, WTI1yr, WTI2yr, WTI3yr, WTI6m))
+#Ohne fehlende Daten
 datCOM2<- subset(datCOM2, select=-c(IronIndex, IronIndex2, PalladiumPrice, SP.Nat.GasIndex.x,SP.Nat.GasIndex.y, IronPrice, SteelIndex, SteelPrice))
 
 #create averages per month
@@ -28,5 +30,25 @@ datCOM_means_m<- na.omit(datCOM_means_m)
 bart_spher(datCOM_means_m[,2:33])
 KMOS(x=datCOM_means_m[,2:33])
 VSS.scree(datCOM_means_m[,2:33])
-pca.d<-principal(datCOM_means_m[,2:33], nfactors=30)
-print(pca.d, sort=T, cut=0.5, digits=2)
+pca.d<-principal(datCOM_means_m[,2:33], nfactors=3)
+print(pca.d, sort=T, cut=0.52, digits=2)
+
+#Run PCA again without the variables that did not fit a component. 
+datCOM_means_m2<- subset(datCOM_means_m, select=-c(CopperIndex, LeadPrice, CopperPrice, ZincPrice, AluPrice, TinIndex))
+
+bart_spher(datCOM_means_m2[,2:27])
+KMOS(x=datCOM_means_m2[,2:27])
+VSS.scree(datCOM_means_m2[,2:27])
+pca.d2<-principal(datCOM_means_m2[,2:27], nfactors=3)
+print(pca.d2, sort=T, cut=0.52, digits=2)
+
+
+round(head(pca.d2$scores,),3)
+dat.scores<- data.frame(pca.d2$scores)
+colnames(dat.scores)<- c("OGI", "Metals", "Energy.Agri")
+
+datcom3 <- cbind(datCOM_means_m2$Group.1, dat.scores)
+colnames(datcom3) <- c("Date","OGI", "Metals", "Energy.Agri")
+
+write_rds(datcom3, "PCA_scores.RDS")
+          
