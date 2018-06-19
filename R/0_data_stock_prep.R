3a84b01e043e1b7682d419c79ffb8d4c582ea23a
Nikolas Kuschnig <kuschnignikolas@gmail.com>
2018-06-19 19:03:25 +0200

    added stock indices

---
+++ R/0_data_stock_prep.R
@@ -0,0 +1,17 @@
+library(readr)
+
+stock_indices <- read_delim("data/datastream/stock_indices.csv", 
+                            ";", escape_double = FALSE, 
+                            col_types = cols(AUS_asx = col_double(), 
+                                             CHL_igpa = col_double(), 
+                                             DEU_dax = col_double(), 
+                                             NOR_obx = col_double(), 
+                                             USA_sp500 = col_double(), 
+                                             ZAF_stock_index = col_double()), 
+                            locale = locale(decimal_mark = ",", 
+                                            grouping_mark = "."), 
+                            trim_ws = TRUE)
+
+names(stock_indices) <- c("DEU", "USA", "NOR", "AUS", "CHL", "ZAF")
+
+saveRDS(stock_indices, "data/raw_data/stock_indices.rds")
