dc6c6530f6c266c534999c8e7fd194a83ef2e000
Casputin <39671075+Casputin@users.noreply.github.com>
2018-06-20 15:03:51 +0200

    PCA_process verändert (commodity_PCA entfernt) Comm_PCA_entfernt datCOM2 gelöscht. Nicht relevanter Subset.

--- R/4_PCA_process.R
+++ R/4_PCA_process.R
@@ -58,13 +58,14 @@ datCOM_means_m4<- na.omit(datCOM_means_m4)
 bart_spher(datCOM_means_m4[,2:13])
 KMOS(x=datCOM_means_m4[,2:13])
 VSS.scree(datCOM_means_m4[,2:13])
-pca.d4<-principal(datCOM_means_m4[,2:13], nfactors=5)
-print(pca.d4, sort=T, cut=0.53, digits=2)
+pca.d4<-principal(datCOM_means_m4[,2:13], nfactors=2)
+print(pca.d4, sort=T, cut=0.55, digits=2)
 
 #Output von scores erstellen für VAR
+#Indices Scores
 round(head(pca.d3$scores),3)
 dat.scores<- data.frame(pca.d3$scores)
-colnames(dat.scores)<- c("En.In.Oi.", "Me.Ag")
+colnames(dat.scores)<- c("me.ag", "en.in.oi.")
 
 datcom3 <- cbind(datCOM_means_m2$Group.1, dat.scores)
 colnames(datcom3) <- c("Date","En.In.Oi.", "Me.Ag")
@@ -74,6 +75,8 @@ write_rds(datcom3, "Ind_PCA.rds")
 datcom3$Date2 <- as.yearqtr(datcom3$Date)
 datcom4<- aggregate(datcom3[,2:4], list(datcom3$Date2), custommean)
 write_rds(datcom4, "Ind_PCA_q.rds")
+
+
 
 
 
